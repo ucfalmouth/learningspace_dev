77733bb646a789babe5f7f9257bfad4dcea567e8
tombola <thomas.readings@gmail.com>
2015-05-22 11:56:14 +0100

    messages

--- provision/shell_provision.sh
+++ provision/shell_provision.sh
@@ -2,6 +2,9 @@
 
 SCRIPTDIR="/var/www/provision/"
 
+echo "Install mdk:"
 source $SCRIPTDIR/install_mdk.sh
+echo "Restore snapshot:"
 source $SCRIPTDIR/restore_snapshot.sh
+echo "Configure apache for moodle:"
 source $SCRIPTDIR/configure_apache.sh
