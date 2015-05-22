e02146df1dec75b52a53c70a63bedbea9a3504c5
tombola <thomas.readings@gmail.com>
2015-05-22 11:02:43 +0100

    also replace form actions (eg search)

--- provision/domain_replace.js
+++ provision/domain_replace.js
@@ -10,15 +10,21 @@
 // ==/UserScript==
 
 document.linkrewrites = 0;
+document.postrewrites = 0;
 
 var linkRewriter = function(a, b) {
     $('a[href*="' + a + '"]').each(function() {
         $(this).attr('href', $(this).attr('href').replace(a, b));
         document.linkrewrites ++;
     });
+    $('form[action*="' + a + '"]').each(function() {
+        $(this).attr('action', $(this).attr('action').replace(a, b));
+        document.postrewrites ++;
+    });
 };
 
 $( document ).ready(function() {
     linkRewriter('learningspace.falmouth.ac.uk', document.domain);
     console.log( document.linkrewrites+" learningspace links rewritten" );
+    console.log( document.postrewrites+" learningspace form actions rewritten" );
 });
