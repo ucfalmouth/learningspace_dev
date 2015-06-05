e2fe6ce852bc64fa2462aff650129a2c64cc29e7
tombola <thomas.readings@gmail.com>
2015-06-05 14:14:32 +0100

    moodle upgrade script

--- provision/shell_provision.sh
+++ provision/shell_provision.sh
@@ -10,5 +10,7 @@ echo "Set username:admin, password:password"
 source $SCRIPTDIR/reset_admin.sh
 echo "Configure apache for moodle:"
 source $SCRIPTDIR/configure_apache.sh
+echo "Upgrade moodle:"
+source $SCRIPTDIR/upgrade_moodle.sh
 
 cat $SCRIPTDIR/bash_profile.sh >> /home/vagrant/.bash_profile
---
+++ provision/upgrade_moodle.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# WEBROOT="/var/www/public"
+# TEMP="/var/www/temp"
+# PLUGINS="/var/www/plugins"
+VERSION="MOODLE_27_STABLE"
+
+
+mkdir -p /var/www/temp
+cp /var/www/public/config.php /var/www/temp
+
+echo "backuping pre-upgrade webroot (to temp folder).."
+tar -pcvzf /var/www/temp/webroot.tar /var/www/public
+echo "removing webroot files.."
+rm -rf /var/www/public/*
+# ensure any hidden files also gone
+rm -rf /var/www/public/.*
+
+echo "cloning moodle core.."
+git clone https://github.com/moodle/moodle.git /var/www/public
+echo "selecting "$VERSION
+git checkout $VERSION
+
+echo "restoring site configuration.."
+cp /var/www/temp/config.php /var/www/public
+
+echo "updating plugins.."
+cp -R /var/www/plugins/$VERSION/mod/* /var/www/public/mod
+cp -R /var/www/plugins/$VERSION/blocks/* /var/www/public/blocks
+cp -R /var/www/plugins/$VERSION/enrol/* /var/www/public/enrol
+cp -R /var/www/plugins/$VERSION/local/* /var/www/public/local
+
+rm /var/www/data/temp
+
+echo "running moodle upgrade script..."
+php admin/cli/upgrade.php
+
+echo "falmouth specific - resetting custom course format.."
+mysql -u root --password=root -e 'UPDATE mdl_course SET format="topics" WHERE format="topicsdeluxe"' scotchbox
